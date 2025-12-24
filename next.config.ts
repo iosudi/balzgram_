c6819bcd00b8848fbeeefd5f8a2a90a0d9dbbbb4
iosudi <saudi33gg@gmail.com>
2025-12-24 21:51:35 +0200

    fix : source and destination config path fix

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: "/hubs/chat",
-        destination: "https://balzgram.runasp.net/hubs/chat",
+        source: "/hubs/chat/:path*",
+        destination: "https://balzgram.runasp.net/hubs/chat/:path*",
       },
     ];
   },
